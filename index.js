eb977eb300701872ff8e578b995c0a8b1c4d615a
André Luís <andreluismoura22@gmail.com>
2024-12-09 20:51:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => res.json({ message: "Welcome!" }))
+app.get('/users', (req, res) => res.json({ message: "List of users" }))
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`👻 Server started at port ${port}`));
