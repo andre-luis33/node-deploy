aabb8d1371f7669ed0d695c0e66273a554fada3d
André Luís <63408314+andre-luis33@users.noreply.github.com>
2024-12-11 20:57:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express'
 
 const app = express()
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.json({ message: "Welcome!" }))
+app.get('/', (req, res) => res.json({ message: `Welcome, the port is ${port}!` }))
 app.get('/users', (req, res) => res.json({ message: "List of users" }))
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`👻 Server started at port ${port}`));
